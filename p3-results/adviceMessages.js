c9ec04ac1f94f3b35d6e0fe5b164d2b3686ccd15
‘Shelby <sls1881@gmail.com>
2021-02-01 10:59:07 -0800

    p3 file tree and comments

---
+++ p3-results/adviceMessages.js
@@ -0,0 +1,6 @@
+//advice message 1
+//underSpent, overSpent, sweetSpot
+
+//advice message 2
+
+//advice message 3
---
+++ p3-results/results.js
@@ -0,0 +1,10 @@
+//get dom elements
+
+//call functions
+
+////event listener for button, on click
+//window.location redirect to p2
+
+////text content and temporal literals for displaying advice
+
+//Use conditional to set message values(results.js in adventure)
