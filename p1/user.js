35c1c6f8e11b6f9f7060253565eef620646ff25d
zhaned <zhanedmond@gmail.com>
2021-02-01 11:00:52 -0800

    created shell for p1 page

---
+++ p1/user.js
@@ -0,0 +1,14 @@
+// import functions and grab DOM elements
+// initialize state
+// set event listeners to update state and DOM
+
+
+//import from localstorage.js
+//grab button element 
+
+// Button Event Listener
+//  Turn monthly expenses into array
+//  Add up all the monthly expenses(gas, groceries, eating out, other)
+//  Take in form data for username, monthly income, monthly expenses, monthly savings and turn into an array
+//  refer to localstorage.js and convert to string for local storage(setuser)
+//  Send to p2
