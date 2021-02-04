0f1ad3724b6e451e9a1963dd6828cd821d385ec1
Cherno Badjan <badjancherno@gmail.com>
2021-02-04 14:55:49 -0800

    messages Updated + some more cleanup

--- p3-results/adviceMessages.js
+++ p3-results/adviceMessages.js
@@ -4,27 +4,35 @@
 export function message(result, expenseName) {
     if (expenseName === 'car') {
         if (result === 'underSpent') {
-            return 'ALERT: We know it is a pandemic, but have you left the house at all?';
+            return 'We know it is a pandemic, but have you left the house at all?';
         } else if (result === 'overSpent') {
-            return 'WARNING: You do not have to drive to the mailbox.';
+            return 'You do not have to drive to the mailbox.';
         } else {
             return 'Not too bad. You are right where you want to be!';
         }
     } else if (expenseName === 'utilities') {
         if (result === 'underSpent') {
-            return 'ALERT: It is not the 1800s, use some electricity.';
+            return 'It is not the 1800s, use some electricity.';
         } else if (result === 'overSpent') {
-            return 'WARNING: You should not run your AC or heat with the windows open.';
+            return 'You should not run your AC or heat with the windows open.';
         } else {
             return 'Nice job, you are right where you want to be!';
         }
     } else if (expenseName === 'other') {
         if (result === 'underSpent') {
-            return 'ALERT: Treat yourself, LIKE please.';
+            return 'Treat yourself, LIKE please.';
         } else if (result === 'overSpent') {
-            return 'WARNING: Close the Amazon app.';
+            return 'Close the Amazon app.';
         } else {
             return 'Great balance of self-care and self-control!';
+        }
+    } else if (expenseName === 'savings') {
+        if (result === 'underSpent') {
+            return 'You will never take a vacation at this rate mate!';
+        } else if (result === 'overSpent') {
+            return 'WOW! Well done you!';
+        } else {
+            return 'Right on target. Keep it up!!';
         }
     } else {
         return '';
